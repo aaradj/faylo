1e54b1e76a4a8dcc6ead2369f08fecbbaaad12f0
Arad Jahangiri <aradjahangiri84@gmail.com>
2023-04-10 21:30:40 +0330

    Add folders and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Main from "./page";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Main />
+    </div>
+  );
 }
 
 export default App;
