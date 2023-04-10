c00f13122b64fca4a0de53a5163bd3fbe013867a
Arad Jahangiri <aradjahangiri84@gmail.com>
2023-04-10 21:14:33 +0330

    Config Prog

---
+++ src/style/color.ts
@@ -0,0 +1,17 @@
+// - Dark Blue (intro and email sign up background): hsl(217, 28%, 15%)
+// - Dark Blue (main background): hsl(218, 28%, 13%)
+// - Dark Blue (footer background): hsl(216, 53%, 9%)
+// - Dark Blue (testimonials background): hsl(219, 30%, 18%)
+// - Cyan (inside call-to-action gradient): hsl(176, 68%, 64%)
+// - Blue (inside call-to-action gradient): hsl(198, 60%, 50%)
+// - Light Red (error): hsl(0, 100%, 63%)
+
+const IntroBg = "hsl(217, 28%, 15%)";
+const MainBg = "hsl(218, 28%, 13%)";
+const TestimonialsBg = "hsl(219, 30%, 18%)";
+const Inside = "hsl(176, 68%, 64%)";
+const Cyan = "hsl(198, 60%, 50%)";
+const Blue = "hsl(198, 60%, 50%)";
+const LightRed = "hsl(0, 100%, 63%)";
+
+export { IntroBg, MainBg, TestimonialsBg, Blue, Cyan, Inside, LightRed };
