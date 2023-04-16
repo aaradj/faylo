c49b85d938b305b2ec1ed35900d6e04b7d1780cd
Arad Jahangiri <aradjahangiri84@gmail.com>
2023-04-16 20:45:22 +0330

    About Done

---
+++ src/components/About/aboutStyle.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+const Main = styled.main`
+  padding: 1rem 0;
+  display: flex;
+  flex-direction: column;
+  height: fit-content;
+  align-items: center;
+  margin-top: 5rem;
+  gap: 2rem;
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    width: 100%;
+    img {
+      width: 50%;
+    }
+  }
+  @media (max-width: 700px) {
+    div {
+      img {
+        width: 100%;
+      }
+    }
+  }
+`;
+
+export { Main } 
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,35 +1,8 @@
-import styled from "styled-components";
 import CHead from "../CHead";
 import CPara from "../CPara";
 import CButton from "../CButton";
+import { Main } from "./aboutStyle";
 
-const Main = styled.main`
-  padding: 1rem 0;
-  display: flex;
-  flex-direction: column;
-  height: fit-content;
-  align-items: center;
-  margin-top: 5rem;
-  gap: 2rem;
-  div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    // text-align:start;
-    gap: 2rem;
-    width: 100%;
-    img {
-      width: 50%;
-    }
-  }
-  @media (max-width: 700px) {
-    div {
-      img {
-        width: 100%;
-      }
-    }
-  }
-`;
 
 const aboutImg = require("../../assets/images/about.png");
 
